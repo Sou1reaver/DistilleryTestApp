0e8335b01f1a3ff4f90bb8857fbde90ee60b5854
Sou1reaver <vladimir.legacy@gmail.com>
2018-01-21 12:01:38 +0300

    commit

--- DistilleryTestApp/BusinessLogicLayer/Services/LocationService/LocationServiceInput.swift
+++ DistilleryTestApp/BusinessLogicLayer/Services/LocationService/LocationServiceInput.swift
@@ -10,7 +10,7 @@ import Foundation
 
 
 protocol LocationServiceInput {
-    weak var output: LocationServiceOutput? {get set}
+    var output: LocationServiceOutput? {get set}
     func requestAuthorization()
     func start()
     func stop() 
